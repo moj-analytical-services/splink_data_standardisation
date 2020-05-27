73dc4cda25c3087d65cd33c0573eb7bb05b6f8a3
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2020-05-27 22:15:53 +0100

    add postcode norm function

---
+++ splink_data_normalisation/postcode.py
@@ -0,0 +1,31 @@
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.functions import expr
+
+def postcode_to_inward_outward(df: DataFrame, pc_field: str, drop_orig:bool = True):
+    """Given a field containing a postcode, creates new columns in the dataframe
+    called outward_postcode_norm and inward_postcode_norm
+
+    Original postcode can have spaces or not and be in any case
+
+    Args:
+        df (DataFrame): Spark Dataframe
+        pc_field (str): Name of field containing postcode
+    """    
+
+    sql = f"replace({pc_field}, ' ', '')"
+    df = df.withColumn("pc_nospace_temp__", expr(sql))
+    
+    sql = f"""left(pc_nospace_temp__, length(pc_nospace_temp__) - 3)"""
+    df = df.withColumn("outward_postcode_norm", expr(sql))
+
+    sql = f"""right(pc_nospace_temp__, 3)"""
+    df = df.withColumn("inward_postcode_norm", expr(sql))
+
+   
+
+    df = df.drop("pc_nospace_temp__")
+
+    if drop_orig:
+        df = df.drop(pc_field)
+
+    return df
---
+++ tests/test_postcode.py
@@ -0,0 +1,37 @@
+import pytest
+import pandas as pd
+
+from splink_data_normalisation.postcode import postcode_to_inward_outward
+from pyspark.sql import Row
+
+
+def test_pc_1(spark):
+
+    postcode_list = [
+        {"id": 1, "postcode": "AB1 2CD"},
+        {"id": 2, "postcode": "AB12CD"},
+        {"id": 3, "postcode": "AB12 3CD"},
+        {"id": 4, "postcode": "AB123CD"},
+        {"id": 5, "postcode": "AB1C 2DE"},
+        {"id": 6, "postcode": "AB1C2DE"},
+        {"id": 7, "postcode": "  AB1 2CD "},
+    ]
+
+    df = spark.createDataFrame(Row(**x) for x in postcode_list)
+    
+    df = postcode_to_inward_outward(df, "postcode")
+    df_result = df.toPandas()
+    
+    expected = [
+        {"id": 1, "outward_postcode_norm": "AB1", "inward_postcode_norm": "2CD"},
+        {"id": 2, "outward_postcode_norm": "AB1", "inward_postcode_norm": "2CD"},
+        {"id": 3, "outward_postcode_norm": "AB12", "inward_postcode_norm": "3CD"},
+        {"id": 4, "outward_postcode_norm": "AB12", "inward_postcode_norm": "3CD"},
+        {"id": 5, "outward_postcode_norm": "AB1C", "inward_postcode_norm": "2DE"},
+        {"id": 6, "outward_postcode_norm": "AB1C", "inward_postcode_norm": "2DE"},
+        {"id": 7, "outward_postcode_norm": "AB1", "inward_postcode_norm": "2CD"}
+    ]
+
+    df_expected = pd.DataFrame(expected)
+
+    pd.testing.assert_frame_equal(df_result,df_expected)
